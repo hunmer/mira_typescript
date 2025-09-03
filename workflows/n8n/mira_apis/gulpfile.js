2fe4f3db2e5d9fd3c0dfea72199dcde15d2ae546
hunmer <liaoyanjie2000@gmail.com>
2025-09-03 17:46:51 +0800

    移除mira-n8n-apis

--- workflows/n8n/mira_apis/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const { src, dest } = require('gulp');
-
-function buildIcons() {
-    return src('nodes/**/*.svg')
-        .pipe(dest('dist/nodes'));
-}
-
-exports.build = buildIcons;
-exports['build:icons'] = buildIcons;
